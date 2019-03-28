c6255f050320efbd05626c356e056a84bea9437e
keke0123 <keke0123@naver.com>
2019-03-28 22:27:06 +0900

    vue-element-admin 과 비동기 통신 / json data

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var jsonData = require('../public/testData/data.json');
+var jsonData2 = require('../public/testData/data2.json');
 
 // cors 관련 테스트
 //var cors = require('cors');
@@ -14,14 +15,42 @@ router.get('/', function(req, res, next) {
   setTimeout(function(){
     res.json(jsonData);
   }, 5000);
-
 });
 
-router.post('/update', function(req, res, next) {
+router.get('/list1', function(req, res, next) {
+  //res.send('respond with a resource');
+  //res.json({msg:'this is cors-enabled'});
+  setTimeout(function(){
+    res.json(jsonData);
+  }, 1000);
+});
+
+router.get('/list2', function(req, res, next) {
+  //res.send('respond with a resource');
+  //res.json({msg:'this is cors-enabled'});
+  setTimeout(function(){
+    res.json(jsonData2);
+  }, 1000);
+});
+
+router.post('/delete', function(req, res, next) {
+  //res.send('respond with a resource');
+  //res.json({msg:'this is cors-enabled'});
+  console.log(req.body);
+  // req.params
+  // req.query
+  // req.body
+  // req.headers
+  res.json({
+    result: 'success'
+  });
+});
+
+router.post('/update2', function(req, res, next) {
   //res.send('respond with a resource');
   //res.json({msg:'this is cors-enabled'});
   console.log(req);
-  res.json(jsonData);
+  res.json(jsonData2);
 });
 
 module.exports = router;
